7fdbe7280452933e570d8915c9ea5fe899e45551
Your-Full-Name <mariarenteria89@gmail.com>
2018-11-27 01:10:37 -0800

    Setup JS file

--- designs.js
+++ designs.js
@@ -1,7 +1,17 @@
 // Select color input
+var color;
+
 // Select size input
+var height, width;
 
 // When size is submitted by the user, call makeGrid()
+$('sizePicker').submit(function (event) {
+  event.preventDefault();
+  height = $('#inputHeight').val();
+  width= $('inputWidth').val();
+  makeGrid(height, width);
+  console.log('Height: ' + height + ' and width: ' + width);
+})
 
 function makeGrid() {
 
