1d7ef1473680e4500e1f60d6519e9ad49e6c9586
Your-Full-Name <mariarenteria89@gmail.com>
2018-11-30 01:24:37 -0800

    Working MouseEvent (Top 11 Lines)

--- designs.js
+++ designs.js
@@ -1,36 +1,47 @@
 // Select color input
-    const colorInput = document.getElementById('colorPicker');
+    let colorInput = document.getElementById('colorPicker');
 
 // Select size input
-    const height = document.getElementById('inputHeight');
-    const width = document.getElementById('inputWidth');
+    let table = document.getElementById('pixelCanvas');
+    let sizePicker = document.getElementById('sizePicker');
+    
+  sizePicker.addEventListener('click', MouseEvent => {
+    MouseEvent.preventDefault();
+    console.log(MouseEvent + 'test');
+  });
+
+
+    //Previous Code below
+    //let height = document.getElementById('inputHeight');
+    //let width = document.getElementById('inputWidth');
+
+
 
 
 // When size is submitted by the user, call makeGrid()
-  $('sizePicker').submit(function (event){
-    event.preventDefault();
-    makeGrid(height,width);
-  })
+  //$('sizePicker').submit(function (event){
+    //makeGrid(height,width);
+  //})
 
-function makeGrid(x,y) {
+//function makeGrid()
     // x= height, and y= width
-    $('tr').remove();
+    //$('tr').remove();
     //tr= table row
 
     // Build Physical Box
-    for (let i = 1; i <= x; i++){
-      $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
-      for (let c = 1; c <=y; c++){
-        $('#table' + i).append('<td></td>');
-      }
-    }
+    //for (let i = 1; i <= x; i++){
+      //$('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+      //for (let c = 1; c <=y; c++){
+        //$('#table' + i).append('<td></td>');
+      //}
+    //}
     //add color to cell
-    $('td').click(function addColor(){
+    //$('td').click(function addColor(){
 
-      if ($(this).attr('style')) {
-          $(this).removeAttr('style')
-      } else {
-          $(this).attr('style', 'background-color;' + color);
-      }
-    })
-}
+      //if ($(this).attr('style')) {
+          //$(this).removeAttr('style')
+      //} else {
+          //$(this).attr('style', 'background-color;' + color);
+      //}
+    //})
+//}
